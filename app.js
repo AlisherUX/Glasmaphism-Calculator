9759b721b415e695948ccc34f3555c39269b535f
alisher <abdirimov110@gmail.com>
2023-06-29 20:19:16 +0500

    alisher

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const boxes = document.querySelectorAll('.calculator-body')
+
+boxes.forEach(box => {
+    box.addEventListener('mousemove', rotate)
+    box.addEventListener('mouseout', remove)
+});
+
+function rotate(e) {
+    console.log(e.offsetX);
+    let halfHeight = this.offsetHeight / 2
+    let x = e.offsetX - halfHeight
+    let y = e.offsetY - halfHeight
+    this.style.transform = `rotateX(${-y / 10}deg) rotateY(${x / 10}deg)`
+
+}
+function remove() {
+    this.style.transform = `rotate(0)`
+}
